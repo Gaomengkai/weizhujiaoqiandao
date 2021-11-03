992bb67aee325bf3ad7054408b84a857a131e22e
Merky Gao <gaomengkai0@outlook.com>
2021-11-03 15:05:05 +0800

    添加 忽略无效openid功能 修复json文件可读性 json在windows下采用GBK编码

--- AsyncCheckIn.py
+++ AsyncCheckIn.py
@@ -1,4 +1,5 @@
-from asyncio.tasks import wait
+# -*-coding:utf-8 -*- 
+import logging
 import random
 import time
 import json
@@ -12,14 +13,15 @@ URL_COURSES = 'https://v18.teachermate.cn/wechat-api/v1/students/courses'
 URL_ACTIVESIGNS = 'https://v18.teachermate.cn/wechat-api/v1/class-attendance/student/active_signs'
 URL_REFERER_T = 'https://v18.teachermate.cn/wechat-pro-ssr/student/sign?openid={}'
 URL_GETNAME_T = 'https://v18.teachermate.cn/wechat-pro-ssr/?openid={}&from=wzj'
-
-XPATH_GETNAME = r'//*[@id="root"]/div/div/div/div[1]/div[1]'
+URL_CHECKINREFER_T = 'https://v18.teachermate.cn/wechat-pro-ssr/student/sign/list/{}'
 
 FILENAME_USEROPENID = "users.json"
 
-TIME_INTERVAL = 45
-RANDOM_BASE = 15
-DELAY_TO_CHECKIN = 0
+TIME_INTERVAL = 60
+RANDOM_BASE = 30
+DELAY_TO_CHECKIN = 13
+LOOPS_COUNTS = 2000
+IGNORE_INVALID_OPENID = 1
 
 UA = [
     r"Mozilla/5.0 (iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.5(0x17000523) NetType/WIFI Language/zh_CN",
@@ -38,20 +40,36 @@ headers = {
     # 'Origin': 'https://v18.teachermate.cn',
 }
 
+
+class ColorPrint:
+    @staticmethod
+    def green(s):
+        return '\033[32m'+str(s)+'\033[0m'
+    @staticmethod
+    def yellow(s):
+        return '\033[33m'+str(s)+'\033[0m'
+    @staticmethod
+    def red(s):
+        return '\033[31m'+str(s)+'\033[0m'
+
+# read user_data into a list
+with open(FILENAME_USEROPENID) as f:
+    userData = json.load(f)
+
+def resave_json_file(j):
+    with open(FILENAME_USEROPENID,'w') as f:
+        json.dump(j,f,indent=4,ensure_ascii=False)
+
 def get_header(host='v18.teachermate.cn', openid='',isCheckin:bool=False,courseId = ''):
     x = headers
     # x['User-Agent'] = random.choice(UA)
     if openid:
         x['openid']=openid
         if isCheckin:
-            x['Referer']='https://v18.teachermate.cn/wechat-pro-ssr/student/sign/list/{}'.format(courseId)
+            x['Referer']=URL_CHECKINREFER_T.format(courseId)
         else:
             x['Referer']=URL_REFERER_T.format(openid)
     return x
-
-# read user_data into a list
-with open(FILENAME_USEROPENID) as f:
-    userData = json.load(f)
 
 
 def check_openid(s,user):
@@ -68,79 +86,55 @@ def renew_openid(s,user):
         openid = input("在这里重新输入您的openid:").strip()
         # Check whether openid is a url or a true id
         if 'teachermate' in openid:
-            # get the true id
             re_ptn = "openid=([^&]*)"
             openid = re.search(re_ptn,openid).group().replace("openid=","")
+
         # Update openid info so that it can be stored in json file
         print(ColorPrint.yellow(f"[{user['name']}] openid已经更新：{openid}"))
         for index in range(len(userData)):
             if userData[index]['name'] == user['name']:
                 userData[index]['openid'] = openid
                 break
-        # re-save json file
-        with open(FILENAME_USEROPENID,"w") as f:
-            json.dump(userData,f)
+        resave_json_file(userData)
         r = s.get(URL_ACTIVESIGNS,headers=get_header(openid=openid))
         is_open_id_ok = r.status_code == 200
     return user
 
-class ColorPrint:
-    @staticmethod
-    def green(s:str):
-        return '\033[32m'+s+'\033[0m'
-    @staticmethod
-    def yellow(s:str):
-        return '\033[33m'+s+'\033[0m'
-    @staticmethod
-    def red(s:str):
-        return '\033[31m'+s+'\033[0m'
 
 async def check_check_in_loop(user):
     s = httpx.Client()
     while not check_openid(s,user):
+        if IGNORE_INVALID_OPENID:
+            print(ColorPrint.red(f"[{user['name']}] 不再询问。"))
+            return
+            break
         user = renew_openid(s,user)
-        print(ColorPrint.green(f"[{user['name']}] 登录信息更新"))
+        print(ColorPrint.green(f"[{user['name']}] 登录信息已经更新"))
     openid = user['openid']
     r = s.get(URL_GETNAME_T.format(openid),headers=get_header(openid=openid))
     re_name = r'name":"([^"]+)'
     new_name = re.search(re_name,r.text)
+    if not new_name:
+        return
     new_name = new_name.group().replace('name":"','')
     print(ColorPrint.green(new_name))
     for index in range(len(userData)):
         if userData[index]['name'] == user['name']:
             userData[index]['name'] = new_name
             break
-    # re-save json file
-    with open(FILENAME_USEROPENID,"w") as f:
-        json.dump(userData,f)
+    resave_json_file(userData)
     user['name'] = new_name
-    for i in range(200):
+
+    for i in range(LOOPS_COUNTS):
         print(f"[{user['name']}] 第{i}次检测")
         r = s.get(URL_ACTIVESIGNS,headers=get_header(openid=openid))
         is_open_id_ok = r.status_code == 200
         while not is_open_id_ok:
             user = renew_openid(s,user)
+            is_open_id_ok = True
             break
-            # print(ColorPrint.red(f"[{user['name']}] 登录信息失效"))
-            # openid = input("在这里重新输入您的openid:").strip()
-            # # Check whether openid is a url or a true id
-            # if 'teachermate' in openid:
-            #     # get the true id
-            #     re_ptn = "openid=([^&]*)"
-            #     openid = re.search(re_ptn,openid).group().replace("openid=","")
-            # # Update openid info so that it can be stored in json file
-            # print(ColorPrint.yellow(f"[{user['name']}] openid已经更新：{openid}"))
-            # for index in range(len(userData)):
-            #     if userData[index]['name'] == user['name']:
-            #         userData[index]['openid'] = openid
-            #         break
-            # # re-save json file
-            # with open(FILENAME_USEROPENID,"w") as f:
-            #     json.dump(userData,f)
-            # r = s.get(URL_ACTIVESIGNS,headers=get_header(openid=openid))
-            # is_open_id_ok = r.status_code == 200
         js = r.json()
-        print(f"[{user['name']}] 有{len(js)}个签到！")
+        print(f"[{user['name']}] 有{ColorPrint.yellow(len(js)) if len(js) > 0 else 0}个签到！")
         for j in js:
             print(f"[{user['name']}] 课堂：{j['name']} 延时{DELAY_TO_CHECKIN}s")
             await asyncio.sleep(DELAY_TO_CHECKIN)
@@ -153,6 +147,7 @@ async def check_check_in_loop(user):
             r = s.post(URL_CHECKIN,data=d,headers=get_header(openid=openid,courseId=courseId,isCheckin=True))
             if 'Rank' in r.text:
                 print(ColorPrint.green(f"-------------[{user['name']}] {j['name']}-签到成功！"))
+                print(f"[{user['name']}] 排名：{ColorPrint.yellow(r.json()['studentRank'])}")
             elif "repeat" in r.text:
                 print(ColorPrint.yellow(f"[{user['name']}] {j['name']}-早已经签到成功！"))
             print(r.text)
